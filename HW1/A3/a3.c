ca52a556b32ee81a91c1cf8cb2cd04da671fac64
shevka07 <e.shevelkina@yandex.ru>
2024-10-25 23:01:09 +0300

    HW1_v3

---
+++ HW1/A3/a3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c,d;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    d = a+b+c;
+    printf("%d+%d+%d=%d\n",a,b,c,d);
+    return 0;
+}
